65d20b7f67381f5711dfd463bb570cfaae918f33
haraphat01 <haraphat@gmail.com>
2023-01-03 19:34:37 +0100

    mis

---
+++ src/Fake/FakeNewsDetector.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import './fake.css';
+
+const FakeNewsDetector = () => {
+  const [headline, setHeadline] = useState('');
+  const [result, setResult] = useState('');
+  const [clear, setClear] = useState('');
+
+  const handleChange = (event) => {
+    setHeadline(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const result = analyzeHeadline(headline);
+    setResult(result);
+  };
+
+  const analyzeHeadline = (headline) => {
+    const keywords = [
+      'this',
+      'number',
+      'question',
+      'see',
+      'why',
+      'what',
+      'piggybacking',
+    ];
+
+    for (const keyword of keywords) {
+      if (headline.toLowerCase().includes(keyword)) {
+        return 'This headline may be a misinformation';
+      }
+    }
+
+    return 'This headline is not a misinformation';
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Enter your Headline
+        <input type="text" value={headline} onChange={handleChange} />
+      </label>
+      <input type="submit" value="Submit" />
+
+      <div>{result}</div>
+    </form>
+  );
+};
+
+export default FakeNewsDetector;
